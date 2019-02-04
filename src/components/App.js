e8111cde9613da91f7fa3302f0ebfc05b516ba9f
xrole <xrolediamond@gmail.com>
2019-02-04 14:31:06 +0100

    [chore:#162727500] Setup empty react project Initialize project with package.json install and configure webpack install and configure babel install and configure eslint install and configure prettier create a dummy component to test [Finishes: #162727500]

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+import ReactDOM from 'react-dom';
+import LandingPage from './landingPage';
+
+class App extends Component{
+  constructor(){
+    super()
+  }
+  render(){
+    return(
+      <LandingPage />
+    )
+  }
+}
+
+export default App;
