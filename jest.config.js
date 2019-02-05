5eaba5a38ef3dae3845c99618a8a0f673f4450fa
Ebenezer <ebenezerdonu@gmail.com>
2019-02-05 13:47:33 +0100

    [#162727503] add snapshotSerializers to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFiles: ['<rootDir>/src/tests/setup.js'],
   transform: { '^.+\\.js$': 'babel-jest' },
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  "snapshotSerializers": ["enzyme-to-json/serializer"],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|svg)$':
       '<rootDir>/__mocks__/fileMock.js',
