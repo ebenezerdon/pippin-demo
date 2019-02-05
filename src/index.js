954eb7e67a768a03a1a0dbd256640fc394e086bf
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-05 15:31:07 +0100

    [#162727501]: update imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import App from './components/App';
+import ReactDOM from 'react-dom';
+import App from './app/App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app')
+);
