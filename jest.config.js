5443c763235eb5b515d687c9a417eb6a4bc5fbf5
Ebenezer <ebenezerdonu@gmail.com>
2019-02-05 13:28:52 +0100

    [#162727503] remove snapshotSerializers from jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   setupFiles: ['<rootDir>/src/tests/setup.js'],
   transform: { '^.+\\.js$': 'babel-jest' },
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  "snapshotSerializers": ["enzyme-to-json/serializer"],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|svg)$':
       '<rootDir>/__mocks__/fileMock.js',
