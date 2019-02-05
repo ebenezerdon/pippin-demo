b20a9b88d865fb01fc58d6f971c656b939483050
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-05 15:32:03 +0100

    [#162727501]: update folder structure

---
+++ src/app/home/HomeContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeComponent from './HomeComponent';
+
+const HomeContainer = () => {
+  return (
+    <HomeComponent />
+  );
+}
+
+export default HomeContainer;
---
+++ src/app/home/index.js
@@ -0,0 +1,2 @@
+export { default as Home } from './HomeContainer';
+export { default as homeReducer } from './duck';
