b5281fefe10323fd34d333fc654c926483fbcae3
habibaudu <auduhabib1990@gmail.com>
2019-02-07 10:30:59 +0100

    feat(navBar):move all style import from App.js to index.js [#163722982]

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import 'materialize-css'
 import { Navbar } from './navBar'
-import '../style/index.scss'
 
 const App = () => (
     <Router>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import 'materialize-css'
 import 'materialize-css/dist/css/materialize.min.css'
 import ReactDOM from 'react-dom'
+import './style/index.scss'
 import App from './app/App'
 
 ReactDOM.render(<App />, document.getElementById('app'))
