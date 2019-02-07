fb6271fe8b2f19ef7d87f9f316a61887464e62a6
habibaudu <auduhabib1990@gmail.com>
2019-02-07 10:12:16 +0100

    feat(Navbar): move materialize import from App.jsx to index.js [#163722982]

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'materialize-css'
-import 'materialize-css/dist/css/materialize.min.css'
 import { Navbar } from './navBar'
 import '../style/index.scss'
 
