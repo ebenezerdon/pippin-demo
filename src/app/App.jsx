00916e9bc7212e3e4d1eb79d00aaee8b5594c29f
habibaudu <auduhabib1990@gmail.com>
2019-02-07 11:37:34 +0100

    feat(navBar) display the home component [#163722982]

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Home } from './home'
 import { Navbar } from './navBar'
 
 const App = () => (
     <Router>
         <Switch>
-            <Route path="/" component={Navbar} />
+            <Navbar />
+            <Route path="/" component={Home} />
         </Switch>
     </Router>
 )
